e3f2ecafd0e6c0ac25c1246954d0b40c0fc5c12c
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2026-03-17 03:36:56 +0400

    Bottom-up approach

--- faberge-easter-eggs-crush-test.py
+++ faberge-easter-eggs-crush-test.py
@@ -1,18 +1,16 @@
-from functools import cache
-
-@cache
-def height(n: int, m: int) -> int:
+def height(n: int, m: int):
+    # T(n, m) = T(n - 1, m - 1) + 1 + T(n, m - 1)
+    
     if n == 0 or m == 0:
         return 0
     
-    if n == 1:
-        return m
+    dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
     
-    if n == 2:
-        return m * (m + 1) // 2
+    for j in range(m + 1):
+        dp[1][j] = j
     
-    ans = 0
-    for i in range(m):
-        ans += height(n - 1, i) + 1
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            dp[i][j] = dp[i - 1][j - 1] + 1 + dp[i][j - 1]
     
-    return ans
+    return dp[n][m]
