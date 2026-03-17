66f900c1fa1834b2cee24573d269d7c0758db36d
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2026-03-18 01:48:42 +0400

    Update faberge-easter-eggs-crush-test.py

--- faberge-easter-eggs-crush-test.py
+++ faberge-easter-eggs-crush-test.py
@@ -7,9 +7,6 @@ def height(n: int, m: int) -> int:
     
     dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
     
-    for j in range(m + 1):
-        dp[1][j] = j
-    
     for i in range(1, n + 1):
         for j in range(1, m + 1):
             dp[i][j] = dp[i - 1][j - 1] + 1 + dp[i][j - 1]
