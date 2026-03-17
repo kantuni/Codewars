d205c977857bec2b38ff8d3e8f8975ec8195cacb
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2026-03-18 01:55:47 +0400

    Update faberge-easter-eggs-crush-test.py

--- faberge-easter-eggs-crush-test.py
+++ faberge-easter-eggs-crush-test.py
@@ -1,14 +1,16 @@
 def height(n: int, m: int) -> int:
     # Recurrence relation:
     # F(n, m) = F(n - 1, m - 1) + 1 + F(n, m - 1)
-    
+
     if n == 0 or m == 0:
         return 0
-    
-    dp = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
-    
+
+    prv = [0 for _ in range(m + 1)]
+    cur = [0 for _ in range(m + 1)]
+
     for i in range(1, n + 1):
         for j in range(1, m + 1):
-            dp[i][j] = dp[i - 1][j - 1] + 1 + dp[i][j - 1]
-    
-    return dp[n][m]
+            cur[j] = prv[j - 1] + 1 + cur[j - 1]
+        prv = cur[:]
+
+    return cur[m]
